66253fead58d7de87b42af738616f4f21c64c5fa
KingKacrae9 <kacrae9@gmail.com>
2026-09-11 15:58:06 +0100

    Assignment completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,57 @@
-## Evidence 2:
-* ### sales summary function
+﻿using Newtonsoft.Json;
 
-using Newtonsoft.Json;
 var currentDirectory = Directory.GetCurrentDirectory();
 var reportDir = Path.Combine(currentDirectory, "Reports");
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);   
+
 var salesFiles = FindFiles(storesDirectory);
+
 var salesTotal = CalculateSalesTotal(salesFiles);
+
 var summaryReport = GenerateReportFile(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+    
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+    
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+    
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+    
+    return salesTotal;
+}
 
 string GenerateReportFile(IEnumerable<String> salesFiles)
 {
@@ -46,3 +91,8 @@ string GenerateReportFile(IEnumerable<String> salesFiles)
     return "Successfully";
 }
 
+
+
+
+
+record SalesData (double Total);
